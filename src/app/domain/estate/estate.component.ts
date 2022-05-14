7c8e94b06fd756e9f5b43af379d85cd99ba94e3f
yeyu83 <chemabravoh@hotmail.com>
2022-05-14 08:11:43 +0200

    feat: estates domain page

--- src/app/domain/estate/estate.component.ts
+++ src/app/domain/estate/estate.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 
 @Component({
-  selector: 'app-estate-detail',
   templateUrl: './estate.component.html',
   styleUrls: ['./estate.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EstateComponent {
 }
--- src/app/domain/estates/estates.component.ts
+++ src/app/domain/estates/estates.component.ts
@@ -1,10 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { Observable } from 'rxjs'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { Estate } from '@hola/models/estate.interface'
+import { EstatesService } from '@hola/services/estates.service'
 
 @Component({
-  selector: 'app-movies',
   templateUrl: './estates.component.html',
   styleUrls: ['./estates.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EstatesComponent {
+export class EstatesComponent implements OnInit {
+  estates$!: Observable<Estate[]>
+
+  constructor(
+    private estatesService: EstatesService,
+  ) { }
+
+  ngOnInit(): void {
+    this.estates$ = this.estatesService.fetchEstates()
+  }
+
+  fetchNextEstates(): void {
+    this.estatesService.fetchNextEstates()
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  trackByFn(_: number, estate: Estate): string {
+    return estate.id
+  }
 }
