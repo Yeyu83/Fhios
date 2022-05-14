e6c49ce3de2ad6808851c8f356e6944f310d1b72
yeyu83 <chemabravoh@hotmail.com>
2022-05-14 14:10:59 +0200

    feat: spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { CoreModule } from './core/core.module'
 import { AppComponent } from './app.component'
+import { SpinnerComponent } from './components/spinner/spinner.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent, SpinnerComponent],
   imports: [
     BrowserModule,
     RouterModule,
     CoreModule,
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
