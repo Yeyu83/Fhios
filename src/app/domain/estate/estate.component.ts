20acb9288d38386d54fcf5bb4ed7e6d32373cf62
yeyu83 <chemabravoh@hotmail.com>
2022-05-14 14:12:18 +0200

    feat: polling app integration

--- src/app/domain/estate/estate.component.ts
+++ src/app/domain/estate/estate.component.ts
@@ -1,7 +1,9 @@
 import { EstatesService } from '@hola/services/estates.service'
-import { Observable, switchMap } from 'rxjs'
+import { Observable, shareReplay, switchMap } from 'rxjs'
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Params } from '@angular/router'
+import { Estate } from '@hola/models/estate.interface'
+import { PollingService } from '@hola/services/polling.service'
 
 @Component({
   templateUrl: './estate.component.html',
@@ -9,18 +11,19 @@ import { ActivatedRoute, Params } from '@angular/router'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EstateComponent implements OnInit {
-  estate$!: Observable<any>
+  estate$!: Observable<Estate>
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly estatesService: EstatesService,
+    public readonly pollingService: PollingService,
   ) { }
 
   ngOnInit(): void {
     this.estate$ = this.route.params
       .pipe(
-        switchMap((params: Params) => this.estatesService
-          .fetchStatesByRef(params['filter[field_inmu_refe]'])),
+        switchMap((params: Params) => this.estatesService.fetchState(params['filter[field_inmu_refe]'])),
+        shareReplay(),
       )
   }
 }
--- src/app/domain/estates/estates.component.ts
+++ src/app/domain/estates/estates.component.ts
@@ -1,5 +1,8 @@
-import { Observable } from 'rxjs'
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { PollingService } from '@hola/services/polling.service'
+import { Observable, shareReplay } from 'rxjs'
+import {
+  ChangeDetectionStrategy, Component, OnDestroy, OnInit,
+} from '@angular/core'
 import { Estate } from '@hola/models/estate.interface'
 import { EstatesService } from '@hola/services/estates.service'
 
@@ -8,23 +11,27 @@ import { EstatesService } from '@hola/services/estates.service'
   styleUrls: ['./estates.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EstatesComponent implements OnInit {
+export class EstatesComponent implements OnInit, OnDestroy {
   estates$!: Observable<Estate[]>
 
   constructor(
-    private estatesService: EstatesService,
+    private readonly estatesService: EstatesService,
+    public readonly pollingService: PollingService,
   ) { }
 
   ngOnInit(): void {
-    this.estates$ = this.estatesService.fetchEstates()
+    this.estates$ = this.estatesService.fetchEstates().pipe(shareReplay())
   }
 
   fetchNextEstates(): void {
     this.estatesService.fetchNextEstates()
   }
 
-  // eslint-disable-next-line class-methods-use-this
   trackByFn(_: number, estate: Estate): string {
     return estate.id
+  }
+
+  ngOnDestroy(): void {
+    this.estatesService.restartPageoffsetCount()
   }
 }
