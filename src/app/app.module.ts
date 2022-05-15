b8ca77336a28526e0639b680adaf7bbac8bf8978
yeyu83 <chemabravoh@hotmail.com>
2022-05-15 08:57:42 +0200

    feat: library ng http cache

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { RouterModule } from '@angular/router'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { NgHttpCachingModule, NgHttpCachingConfig, NgHttpCachingStrategy } from 'ng-http-caching'
+import { SpinnerComponent } from '@hola/components/spinner/spinner.component'
 import { CoreModule } from './core/core.module'
 import { AppComponent } from './app.component'
-import { SpinnerComponent } from './components/spinner/spinner.component'
+
+const ngHttpCachingConfig: NgHttpCachingConfig = {
+  lifetime: 1000 * 240,
+  allowedMethod: ['GET', 'HEAD'],
+  cacheStrategy: NgHttpCachingStrategy.ALLOW_ALL,
+}
 
 @NgModule({
   declarations: [AppComponent, SpinnerComponent],
@@ -11,6 +18,7 @@ import { SpinnerComponent } from './components/spinner/spinner.component'
     BrowserModule,
     RouterModule,
     CoreModule,
+    NgHttpCachingModule.forRoot(ngHttpCachingConfig),
   ],
   bootstrap: [AppComponent],
 })
